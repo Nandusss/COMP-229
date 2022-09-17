a5f8924270e8a774a12f0a6ce1d80fdf2caf2183
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-09-17 11:36:46 -0400

    Update index.js

--- ArrowFunction/index.js
+++ ArrowFunction/index.js
@@ -9,3 +9,8 @@ window.addEventListener("load", hello);
 
 //A button object calls the function:
 document.getElementById("btn").addEventListener("click", hello);
+
+//A button object calls the function:
+document.getElementById("btn2").addEventListener("click", () => {
+    document.getElementById("demo").innerHTML += "Hello 2";
+});
