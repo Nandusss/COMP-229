33e72654587c4ea86661ca6237cc26e1e639f95d
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-08 22:25:44 -0400

    projects page

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/about', function(req, res, next) {
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 router.get('/services', function(req, res, next) {
