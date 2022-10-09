e27f39b3060378b727a16329d853e55bef0da726
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-08 23:41:56 -0400

    final

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/services', function(req, res, next) {
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });
 });
 
 
