f2c525e396016a032aa4538580e549f052489724
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-08 23:43:55 -0400

    Update about.js

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -18,7 +18,7 @@ else if(window.attachEvent)
 }
 
 function main() {
-    var msgs =  "Hi. I am Nandagopan Dilip. Currntly enrolled at Centennial College in the program Software Engineering Technology - Artificial Intelligence program. \n \n I have skills in the following fields: \n \t Software Engineering \n \t Web Developement \n \t Programming \n"
+    var msgs =  "Hi. I am Nandagopan Dilip. Currently enrolled at Centennial College in the program Software Engineering Technology - Artificial Intelligence program. \n \n I have skills in the following fields: \n \t Software Engineering \n \t Web Developement \n \t Programming \n"
     var speed = 250;
     var typeArea = document.getElementById("self-points");
     showWordByWord(typeArea, msgs, speed);
