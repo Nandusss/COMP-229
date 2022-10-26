0f1c39f26ad77bdd612ee9b6ba41963a3047f0c5
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-25 21:30:27 -0400

    fix db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
 // Exposing the credentials only for academic purposes, Please do not follow this practise in real world functionalities
-let dbURI = "mongodb+srv:LSIeKcwTMG0hfwDd//dbuser:@cluster1.m8kzbry.mongodb.net/products?retryWrites=true&w=majority";
+let dbURI = "mongodb+srv://dbuser:LSIeKcwTMG0hfwDd@cluster1.m8kzbry.mongodb.net/products?retryWrites=true&w=majority";
 
 let mongoose = require('mongoose');
 
 module.exports = function() {
     //connecting to the database
     mongoose.connect(dbURI);
-
+    
     let mongoDB = mongoose.connection;
+    
+
 
     mongoDB.once('open', () => {
             console.log('=== Connected To DataBase Successfully ===');
         }
     )
+
+    return mongoDB;
 }
