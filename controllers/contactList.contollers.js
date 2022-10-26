c6e3d0faf9340b6ab45a45ca55db689da2c65959
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-26 02:37:33 -0400

    Update contactList.contollers.js

--- controllers/contactList.contollers.js
+++ controllers/contactList.contollers.js
@@ -22,3 +22,113 @@ module.exports.contactList = function(req, res, next) {
     );
 }
 
+// Edit the values using add_edit page
+module.exports.displayEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    contactList.findById(
+        id, (err, itemToEdit) => {
+            if(err)
+            {
+                console.log(err);
+                res.end(err);
+            }
+            else
+            {
+                //show the edit view
+                res.render(
+                    'contactlist/add_edit', {
+                        title: 'Edit Item', 
+                        item: itemToEdit
+                    }
+                )
+            }
+        }
+    );
+}
+
+// perform an update operation on database from edit page
+module.exports.processEditPage = (req, res, next) => {
+    let id = req.params.id
+
+    let updatedItem = contactList({
+        _id: req.body.id,
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email,
+    });
+
+    contactList.updateOne(
+        {_id: id}, updatedItem, (err) => {
+            if(err)
+            {
+                console.log(err);
+                res.end(err);
+            }
+            else
+            {
+                // refresh the contacts list
+                res.redirect('/contactlist/list');
+            }
+        }
+    );
+}
+
+// Display add_edit page
+module.exports.displayAddPage = (req, res, next) => {
+    let newItem = contactList();
+
+    res.render(
+        'contactlist/add_edit', {
+            title: 'Add a new Item',
+            item: newItem
+        }
+    )          
+}
+
+//perform a create function over database
+module.exports.processAddPage = (req, res, next) => {
+    let newItem = contactList({
+        _id: req.body.id,
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email,
+    });
+
+    contactList.create(
+        newItem, (err, item) =>{
+            if(err)
+            {
+                console.log(err);
+                res.end(err);
+            }
+            else
+            {
+                // refresh the contact list
+                console.log(item);
+                res.redirect('/contactlist/list');
+            }
+        }
+    );
+
+}
+
+//perform a delete fuction over database
+module.exports.performDelete = (req, res, next) => {
+    let id = req.params.id;
+
+    contactList.remove(
+        {_id: id}, (err) => {
+            if(err)
+            {
+                console.log(err);
+                res.end(err);
+            }
+            else
+            {
+                // refresh the contact list
+                res.redirect('/contactlist/list');
+            }
+        }
+    );
+}
