3af2c7e24852aac9089457abe95cd2db307d297f
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-25 22:02:59 -0400

    controller established

---
+++ controllers/index.controllers.js
@@ -0,0 +1,45 @@
+//controller which controlls all the pages that are displayed
+exports.home = function(req, res, next) {
+    res.render(
+        'index', 
+        { 
+            title: 'Home'
+        }
+    );
+  }
+
+exports.about = function(req, res, next) {
+    res.render(
+        'about', 
+        { 
+            title: 'About' 
+        }
+    );
+  }
+
+  exports.projects = function(req, res, next) {
+    res.render(
+        'projects', 
+        {
+            title: 'Projects' 
+        }
+    );
+  }
+
+  exports.services = function(req, res, next) {
+    res.render(
+        'services',
+        { 
+            title: 'Services' 
+        }
+    );
+  }
+
+  exports.contact = function(req, res, next) {
+    res.render(
+        'contact', 
+        { 
+            title: 'Contact' 
+        }
+    );
+  }
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
+// Controller
+let indexController = require('../controllers/index.controllers');
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
-});
+/* GET all necessary pages. */
+router.get('/', indexController.home);
 
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
-});
+router.get('/about', indexController.about);
 
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
-});
+router.get('/projects', indexController.projects);
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
-});
+router.get('/services', indexController.services);
+
+router.get('/contact', indexController.contact);
 
 
 module.exports = router;
