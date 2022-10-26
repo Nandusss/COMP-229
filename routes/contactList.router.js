58c95f122bfd67df9d2315079a4ae71a322c7118
Nandagopan Dilip <113753010+Nandusss@users.noreply.github.com>
2022-10-26 03:04:17 -0400

    Contact list fully functional

--- routes/contactList.router.js
+++ routes/contactList.router.js
@@ -5,9 +5,24 @@ var router = express.Router();
 let contactListController = require('../controllers/contactList.contollers');
 
 
-/* GET all necessary pages. */
+/* GET all the contact list */
 router.get('/', contactListController.contactList);
-
 router.get('/list', contactListController.contactList);
+
+// Routers for edit contact
+router.get('/edit/:id', contactListController.displayEditPage);
+router.post('/edit/:id', contactListController.processEditPage);
+
+
+/* GET Route for displaying the Add page - CREATE Operation */
+router.get('/add', contactListController.displayAddPage);
+
+/* POST Route for processing the Add page - CREATE Operation */
+router.post('/add', contactListController.processAddPage);
+
+
+// Route for Delete
+router.get('/delete/:id', contactListController.performDelete);
+
 
 module.exports = router;
